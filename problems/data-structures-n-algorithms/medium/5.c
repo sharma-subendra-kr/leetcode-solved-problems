4fc8e8aaaaa22a623bc8431ea6b853bde0f9f6c0
Subendra Sharma <sharma.subendra.kr@gmail.com>
2020-11-18 15:30:21 +0530

    update

--- problems/data-structures-n-algorithms/medium/5.c
+++ problems/data-structures-n-algorithms/medium/5.c
@@ -5,11 +5,93 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// char *longestPalindrome(char *s) {
+// 	int i = 0;
+// 	int len = 0, low, high, maxLen = 0, currLen = 0, where = 0, iFront, iBack,
+// tF, 			tB; 	int **map, count = 0; 	char *res;
+
+// 	while (s[len] != '\0') {
+// 		len++;
+// 	}
+
+// 	if (len == 0 || len == 1) {
+// 		return s;
+// 	} else if (len == 2 && s[0] == s[1]) {
+// 		return s;
+// 	} else if (len == 2) {
+// 		res = malloc(sizeof(char) * 2);
+// 		res[0] = s[0];
+// 		res[1] = '\0';
+// 		return res;
+// 	}
+
+// 	map = (int **)malloc(sizeof(int *) * len * 2);
+
+// 	for (i = 1; i < len - 1; i++) {
+// 		if (s[i] == s[i - 1]) {
+// 			map[count] = (int *)malloc(sizeof(int) * 2);
+// 			map[count][0] = i - 1;
+// 			map[count][1] = i;
+// 			count++;
+// 		} else if (s[i] == s[i + 1]) {
+// 			map[count] = (int *)malloc(sizeof(int) * 2);
+// 			map[count][0] = i;
+// 			map[count][1] = i + 1;
+// 			count++;
+// 		}
+// 		if (s[i - 1] == s[i + 1]) {
+// 			map[count] = (int *)malloc(sizeof(int) * 2);
+// 			map[count][0] = i - 1;
+// 			map[count][1] = i + 1;
+// 			count++;
+// 		}
+// 	}
+
+// 	if (count == 0) {
+// 		res = malloc(sizeof(char) * 2);
+// 		res[0] = s[0];
+// 		res[1] = '\0';
+// 		return res;
+// 	}
+
+// 	for (i = 0; i < count; i++) {
+// 		low = map[i][0];
+// 		high = map[i][1];
+// 		currLen = map[i][1] - map[i][0] + 1;
+// 		while (low >= 0 && high < len) {
+// 			if (s[low] == s[high]) {
+// 				currLen += 2;
+// 				tF = low;
+// 				tB = high;
+// 				low--;
+// 				high++;
+// 			} else {
+// 				break;
+// 			}
+// 		}
+// 		if (currLen >= maxLen) {
+// 			where = i;
+// 			maxLen = currLen;
+// 			iFront = tF;
+// 			iBack = tB;
+// 		}
+// 	}
+
+// 	count = 0;
+// 	res = (char *)malloc(sizeof(char) * (maxLen + 1));
+
+// 	for (i = iFront; i <= iBack; i++) {
+// 		res[count++] = s[i];
+// 	}
+
+// 	res[count] = '\0';
+
+// 	return res;
+// }
+
 char *longestPalindrome(char *s) {
 	int i = 0;
-	int len = 0, low, high, maxLen = 0, currLen = 0, where = 0, iFront, iBack, tF,
-			tB;
-	int **map, count = 0;
+	int len = 0, low, high, maxLen = 1, currLen = 0, start = 0, count = 0;
 	char *res;
 
 	while (s[len] != '\0') {
@@ -27,62 +109,48 @@ char *longestPalindrome(char *s) {
 		return res;
 	}
 
-	map = (int **)malloc(sizeof(int *) * len * 2);
-
-	for (i = 1; i < len - 1; i++) {
-		if (s[i] == s[i - 1]) {
-			map[count] = (int *)malloc(sizeof(int) * 2);
-			map[count][0] = i - 1;
-			map[count][1] = i;
-			count++;
-		} else if (s[i] == s[i + 1]) {
-			map[count] = (int *)malloc(sizeof(int) * 2);
-			map[count][0] = i;
-			map[count][1] = i + 1;
-			count++;
+	for (i = 1; i < len; i++) {
+		low = i - 1;
+		high = i;
+		currLen = 0;
+		while (low >= 0 && high < len && s[low] == s[high]) {
+			low--;
+			high++;
+			currLen += 2;
 		}
-		if (s[i - 1] == s[i + 1]) {
-			map[count] = (int *)malloc(sizeof(int) * 2);
-			map[count][0] = i - 1;
-			map[count][1] = i + 1;
-			count++;
-		}
-	}
 
-	if (count == 0) {
-		res = malloc(sizeof(char) * 2);
-		res[0] = s[0];
-		res[1] = '\0';
-		return res;
-	}
-
-	for (i = 0; i < count; i++) {
-		low = map[i][0];
-		high = map[i][1];
-		currLen = map[i][1] - map[i][0] + 1;
-		while (low >= 0 && high < len) {
-			if (s[low] == s[high]) {
-				currLen += 2;
-				tF = low;
-				tB = high;
-				low--;
-				high++;
-			} else {
-				break;
-			}
-		}
-		if (currLen >= maxLen) {
-			where = i;
+		if (currLen > maxLen) {
+			start = low + 1;
 			maxLen = currLen;
-			iFront = tF;
-			iBack = tB;
+		}
+
+		if (i == len - 1) {
+			continue;
+		}
+
+		low = i - 1;
+		high = i + 1;
+		currLen = 0;
+		while (low >= 0 && high < len && s[low] == s[high]) {
+			if (low == i - 1) {
+				currLen += 3;
+			} else {
+				currLen += 2;
+			}
+
+			low--;
+			high++;
+		}
+
+		if (currLen > maxLen) {
+			start = low + 1;
+			maxLen = currLen;
 		}
 	}
 
-	count = 0;
 	res = (char *)malloc(sizeof(char) * (maxLen + 1));
 
-	for (i = iFront; i <= iBack; i++) {
+	for (i = start; i <= start + maxLen - 1; i++) {
 		res[count++] = s[i];
 	}
 
