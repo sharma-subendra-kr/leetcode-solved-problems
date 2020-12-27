d78c8f8c210b29717118bec9e608eebf789bec1a
Subendra Sharma <sharma.subendra.kr@gmail.com>
2020-12-27 18:05:39 +0530

    breaking the list

--- problems/data-structures-n-algorithms/medium/148.cpp
+++ problems/data-structures-n-algorithms/medium/148.cpp
@@ -16,13 +16,92 @@ struct ListNode {
 	ListNode(int x, ListNode *next) : val(x), next(next) {}
 };
 
-ListNode **merge(ListNode *list, ListNode *left, ListNode *leftTail,
-								 ListNode *right, ListNode *rightTail) {
+// ListNode **merge(ListNode *list, ListNode *left, ListNode *leftTail,
+// 								 ListNode *right, ListNode *rightTail) {
+// 	ListNode *res = new ListNode(), *iter = NULL;
+// 	iter = res;
+
+// 	while ((left != leftTail && left != NULL) &&
+// 				 (right != rightTail && right != NULL)) {
+// 		if (left->val < right->val) {
+// 			iter->next = left;
+// 			left = left->next;
+// 		} else {
+// 			iter->next = right;
+// 			right = right->next;
+// 		}
+// 		iter = iter->next;
+// 	}
+
+// 	while (left != leftTail && left != NULL) {
+// 		iter->next = left;
+// 		left = left->next;
+// 		iter = iter->next;
+// 	}
+
+// 	while (right != rightTail && right != NULL) {
+// 		iter->next = right;
+// 		right = right->next;
+// 		iter = iter->next;
+// 	}
+
+// 	iter->next = rightTail;
+
+// 	ListNode **arr = new ListNode *[2];
+// 	arr[0] = res->next;
+// 	arr[1] = rightTail;
+// 	return arr;
+// }
+
+// ListNode **mergeSort(ListNode *list, ListNode *head, ListNode *tail) {
+// 	ListNode *fast = head, *slow = head, *mid;
+
+// 	if (head == tail || head == NULL) {
+// 		ListNode **arr = new ListNode *[2];
+// 		arr[0] = head;
+// 		arr[1] = head == NULL ? NULL : head->next;
+// 		return arr;
+// 	}
+
+// 	if (head->next != tail) {
+// 		while (fast != tail) {
+// 			slow = slow->next;
+// 			fast = fast->next;
+// 			if (fast != tail) {
+// 				fast = fast->next;
+// 			}
+// 		}
+// 	} else {
+// 		slow = head;
+// 	}
+
+// 	mid = slow;
+
+// 	ListNode **left = mergeSort(list, head, mid);
+// 	ListNode **right = mergeSort(list, left[1], tail);
+// 	ListNode **res = merge(list, left[0], left[1], right[0], right[1]);
+// 	return res;
+// }
+
+// ListNode *sortList(ListNode *head) {
+// 	ListNode *tail = head;
+
+// 	if (head == NULL) {
+// 		return head;
+// 	}
+
+// 	while (tail->next != NULL) {
+// 		tail = tail->next;
+// 	}
+
+// 	return mergeSort(head, head, tail)[0];
+// }
+
+ListNode *merge(ListNode *list, ListNode *left, ListNode *right) {
 	ListNode *res = new ListNode(), *iter = NULL;
 	iter = res;
 
-	while ((left != leftTail && left != NULL) &&
-				 (right != rightTail && right != NULL)) {
+	while (left != NULL && right != NULL) {
 		if (left->val < right->val) {
 			iter->next = left;
 			left = left->next;
@@ -33,34 +112,28 @@ ListNode **merge(ListNode *list, ListNode *left, ListNode *leftTail,
 		iter = iter->next;
 	}
 
-	while (left != leftTail && left != NULL) {
+	while (left != NULL) {
 		iter->next = left;
 		left = left->next;
 		iter = iter->next;
 	}
 
-	while (right != rightTail && right != NULL) {
+	while (right != NULL) {
 		iter->next = right;
 		right = right->next;
 		iter = iter->next;
 	}
 
-	iter->next = rightTail;
+	iter->next = NULL;
 
-	ListNode **arr = new ListNode *[2];
-	arr[0] = res->next;
-	arr[1] = rightTail;
-	return arr;
+	return res->next;
 }
 
-ListNode **mergeSort(ListNode *list, ListNode *head, ListNode *tail) {
-	ListNode *fast = head, *slow = head, *mid;
+ListNode *mergeSort(ListNode *list, ListNode *head, ListNode *tail) {
+	ListNode *fast = head, *slow = head, *mid, *next;
 
 	if (head == tail || head == NULL) {
-		ListNode **arr = new ListNode *[2];
-		arr[0] = head;
-		arr[1] = head == NULL ? NULL : head->next;
-		return arr;
+		return head;
 	}
 
 	if (head->next != tail) {
@@ -76,10 +149,12 @@ ListNode **mergeSort(ListNode *list, ListNode *head, ListNode *tail) {
 	}
 
 	mid = slow;
+	next = mid->next;
+	mid->next = NULL;
 
-	ListNode **left = mergeSort(list, head, mid);
-	ListNode **right = mergeSort(list, left[1], tail);
-	ListNode **res = merge(list, left[0], left[1], right[0], right[1]);
+	ListNode *left = mergeSort(list, head, mid);
+	ListNode *right = mergeSort(list, next, tail);
+	ListNode *res = merge(list, left, right);
 	return res;
 }
 
@@ -94,7 +169,7 @@ ListNode *sortList(ListNode *head) {
 		tail = tail->next;
 	}
 
-	return mergeSort(head, head, tail)[0];
+	return mergeSort(head, head, tail);
 }
 
 int main() {
