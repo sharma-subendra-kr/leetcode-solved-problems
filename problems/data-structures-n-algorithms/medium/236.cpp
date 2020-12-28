131fc2f0d729323048cac79f44a44b26e1e4f262
Subendra Sharma <sharma.subendra.kr@gmail.com>
2020-12-28 22:59:27 +0530

    using pairs

--- problems/data-structures-n-algorithms/medium/236.cpp
+++ problems/data-structures-n-algorithms/medium/236.cpp
@@ -5,6 +5,7 @@
 #include <deque>
 #include <iostream>
 #include <vector>
+#include <utility>
 #include <cstdlib>
 
 using namespace std;
@@ -18,39 +19,105 @@ struct TreeNode {
 
 #include "../../../utils/print.h"
 
-struct Item {
-	struct TreeNode *node;
-	int status;
-	Item() : status(0), node(NULL) {}
-	Item(int s, TreeNode *n) : status(s), node(n) {}
-};
+// struct Item {
+// 	struct TreeNode *node;
+// 	int status;
+// 	Item() : status(0), node(NULL) {}
+// 	Item(int s, TreeNode *n) : status(s), node(n) {}
+// };
 
-stack<Item *> traverse(TreeNode *root, TreeNode *node) {
-	stack<Item *> s;
-	Item *curr;
+// stack<Item *> traverse(TreeNode *root, TreeNode *node) {
+// 	stack<Item *> s;
+// 	Item *curr;
 
-	Item *item = new Item(0, root);
+// 	Item *item = new Item(0, root);
+// 	s.push(item);
+
+// 	while (!s.empty()) {
+// 		curr = s.top();
+
+// 		if (curr->node == node) {
+// 			return s;
+// 		}
+
+// 		curr->status++;
+
+// 		if (curr->status > 2) {
+// 			s.pop();
+// 			continue;
+// 		}
+
+// 		if (curr->status == 1 && curr->node->left != NULL) {
+// 			Item *temp = new Item(0, curr->node->left);
+// 			s.push(temp);
+// 		} else if (curr->node->right != NULL) {
+// 			Item *temp = new Item(0, curr->node->right);
+// 			s.push(temp);
+// 		}
+// 	}
+
+// 	return s;
+// }
+
+// TreeNode *lowestCommonAncestor(TreeNode *root, TreeNode *p, TreeNode *q) {
+// 	stack<Item *> ps = traverse(root, p);
+// 	stack<Item *> qs = traverse(root, q);
+// 	stack<Item *> *curr = NULL;
+// 	Item *ip = NULL, *iq = NULL;
+
+// 	int diff = abs((int)(ps.size() - qs.size()));
+// 	if (ps.size() > qs.size()) {
+// 		curr = &ps;
+// 	} else if (qs.size() > ps.size()) {
+// 		curr = &qs;
+// 	}
+
+// 	while (diff != 0) {
+// 		curr->pop();
+// 		diff--;
+// 	}
+
+// 	while (ps.size() > 0 && qs.size() > 0) {
+// 		ip = ps.top();
+// 		ps.pop();
+
+// 		iq = qs.top();
+// 		qs.pop();
+
+// 		if (ip->node->val == iq->node->val) {
+// 			return ip->node;
+// 		}
+// 	}
+
+// 	return NULL;
+// }
+
+stack<pair<TreeNode *, int>> traverse(TreeNode *root, TreeNode *node) {
+	stack<pair<TreeNode *, int>> s;
+	pair<TreeNode *, int> *curr;
+
+	pair<TreeNode *, int> item(root, 0);
 	s.push(item);
 
 	while (!s.empty()) {
-		curr = s.top();
+		curr = &s.top();
 
-		if (curr->node == node) {
+		if (curr->first == node) {
 			return s;
 		}
 
-		curr->status++;
+		curr->second++;
 
-		if (curr->status > 2) {
+		if (curr->second > 2) {
 			s.pop();
 			continue;
 		}
 
-		if (curr->status == 1 && curr->node->left != NULL) {
-			Item *temp = new Item(0, curr->node->left);
+		if (curr->second == 1 && curr->first->left != NULL) {
+			pair<TreeNode *, int> temp(curr->first->left, 0);
 			s.push(temp);
-		} else if (curr->node->right != NULL) {
-			Item *temp = new Item(0, curr->node->right);
+		} else if (curr->first->right != NULL) {
+			pair<TreeNode *, int> temp(curr->first->right, 0);
 			s.push(temp);
 		}
 	}
@@ -59,10 +126,10 @@ stack<Item *> traverse(TreeNode *root, TreeNode *node) {
 }
 
 TreeNode *lowestCommonAncestor(TreeNode *root, TreeNode *p, TreeNode *q) {
-	stack<Item *> ps = traverse(root, p);
-	stack<Item *> qs = traverse(root, q);
-	stack<Item *> *curr = NULL;
-	Item *ip = NULL, *iq = NULL;
+	stack<pair<TreeNode *, int>> ps = traverse(root, p);
+	stack<pair<TreeNode *, int>> qs = traverse(root, q);
+	stack<pair<TreeNode *, int>> *curr = NULL;
+	pair<TreeNode *, int> ip, iq;
 
 	int diff = abs((int)(ps.size() - qs.size()));
 	if (ps.size() > qs.size()) {
@@ -83,8 +150,8 @@ TreeNode *lowestCommonAncestor(TreeNode *root, TreeNode *p, TreeNode *q) {
 		iq = qs.top();
 		qs.pop();
 
-		if (ip->node->val == iq->node->val) {
-			return ip->node;
+		if (ip.first->val == iq.first->val) {
+			return ip.first;
 		}
 	}
 
@@ -94,47 +161,47 @@ TreeNode *lowestCommonAncestor(TreeNode *root, TreeNode *p, TreeNode *q) {
 int main() {
 	TreeNode *head;
 
-	head = new TreeNode(3);
+	// head = new TreeNode(3);
 
-	TreeNode *node1 = new TreeNode(5);
-	head->left = node1;
+	// TreeNode *node1 = new TreeNode(5);
+	// head->left = node1;
 
-	TreeNode *node2 = new TreeNode(1);
-	head->right = node2;
+	// TreeNode *node2 = new TreeNode(1);
+	// head->right = node2;
 
-	TreeNode *node3 = new TreeNode(6);
-	node1->left = node3;
+	// TreeNode *node3 = new TreeNode(6);
+	// node1->left = node3;
 
-	TreeNode *node4 = new TreeNode(2);
-	node1->right = node4;
+	// TreeNode *node4 = new TreeNode(2);
+	// node1->right = node4;
 
-	TreeNode *node5 = new TreeNode(0);
-	node2->left = node5;
+	// TreeNode *node5 = new TreeNode(0);
+	// node2->left = node5;
 
-	TreeNode *node6 = new TreeNode(8);
-	node2->right = node6;
+	// TreeNode *node6 = new TreeNode(8);
+	// node2->right = node6;
 
-	TreeNode *node7 = new TreeNode(7);
-	node4->left = node7;
+	// TreeNode *node7 = new TreeNode(7);
+	// node4->left = node7;
 
-	TreeNode *node8 = new TreeNode(4);
-	node4->right = node8;
+	// TreeNode *node8 = new TreeNode(4);
+	// node4->right = node8;
 
 	//
 
-	// head = new TreeNode(1);
-	// TreeNode *node0 = head;
+	head = new TreeNode(1);
+	TreeNode *node0 = head;
 
-	// TreeNode *node1 = new TreeNode(2);
-	// head->left = node1;
+	TreeNode *node1 = new TreeNode(2);
+	head->left = node1;
 
-	// TreeNode *node2 = new TreeNode(3);
-	// head->right = node2;
+	TreeNode *node2 = new TreeNode(3);
+	head->right = node2;
 
-	// TreeNode *node3 = new TreeNode(4);
-	// node1->right = node3;
+	TreeNode *node3 = new TreeNode(4);
+	node1->right = node3;
 
-	TreeNode *res = lowestCommonAncestor(head, node1, node8);
+	TreeNode *res = lowestCommonAncestor(head, node0, node3);
 
 	cout << res->val;
 
