0923827d5555d0ea3373549a6a95a67a9e1ebea1
Subendra Sharma <sharma.subendra.kr@gmail.com>
2020-12-29 19:29:47 +0530

    update

--- problems/data-structures-n-algorithms/hard/124.cpp
+++ problems/data-structures-n-algorithms/hard/124.cpp
@@ -44,66 +44,87 @@ struct TreeNode {
 // 	return m3;
 // };
 
-int traverse(TreeNode *root) {
-	stack<pair<TreeNode *, int>> s;
-	unordered_map<TreeNode *, int> mp;
+// int traverse(TreeNode *root) {
+// 	stack<pair<TreeNode *, int>> s;
+// 	unordered_map<TreeNode *, int> mp;
 
-	pair<TreeNode *, int> *curr;
+// 	pair<TreeNode *, int> *curr;
 
-	int max = INT_MIN;
+// 	int max = INT_MIN;
 
-	pair<TreeNode *, int> item(root, 0);
-	s.push(item);
-	mp.insert({root, root->val});
+// 	pair<TreeNode *, int> item(root, 0);
+// 	s.push(item);
+// 	mp.insert({root, root->val});
 
-	while (!s.empty()) {
-		curr = &s.top();
+// 	while (!s.empty()) {
+// 		curr = &s.top();
 
-		curr->second++;
+// 		curr->second++;
 
-		if (curr->second > 2) {
-			s.pop();
+// 		if (curr->second > 2) {
+// 			s.pop();
 
-			int sum = curr->first->val;
-			int sumL = sum, sumR = sum, sumAll = sum, sumMax;
+// 			int sum = curr->first->val;
+// 			int sumL = sum, sumR = sum, sumAll = sum, sumMax;
 
-			if (curr->first->left != NULL) {
-				auto iter = mp.find(curr->first->left);
-				sumL = sum + iter->second;
-				sumAll = sumAll + iter->second;
-				mp.erase(curr->first->left);
-			}
-			if (curr->first->right != NULL) {
-				auto iter = mp.find(curr->first->right);
-				sumR = sum + iter->second;
-				sumAll = sumAll + iter->second;
-				mp.erase(curr->first->right);
-			}
+// 			if (curr->first->left != NULL) {
+// 				auto iter = mp.find(curr->first->left);
+// 				sumL = sum + iter->second;
+// 				sumAll = sumAll + iter->second;
+// 				mp.erase(curr->first->left);
+// 			}
+// 			if (curr->first->right != NULL) {
+// 				auto iter = mp.find(curr->first->right);
+// 				sumR = sum + iter->second;
+// 				sumAll = sumAll + iter->second;
+// 				mp.erase(curr->first->right);
+// 			}
 
-			sumMax = std::max({sum, sumL, sumR});
-			sumAll = std::max({sum, sumL, sumR, sumAll});
+// 			sumMax = std::max({sum, sumL, sumR});
+// 			sumAll = std::max({sum, sumL, sumR, sumAll});
 
-			auto f = mp.find(curr->first);
-			f->second = sumMax;
+// 			auto f = mp.find(curr->first);
+// 			f->second = sumMax;
 
-			if (sumAll > max) {
-				max = sumAll;
-			}
-			continue;
-		}
+// 			if (sumAll > max) {
+// 				max = sumAll;
+// 			}
+// 			continue;
+// 		}
 
-		if (curr->second == 1 && curr->first->left != NULL) {
-			pair<TreeNode *, int> temp(curr->first->left, 0);
-			s.push(temp);
-			mp.insert({curr->first->left, curr->first->left->val});
-		} else if (curr->first->right != NULL) {
-			pair<TreeNode *, int> temp(curr->first->right, 0);
-			s.push(temp);
-			mp.insert({curr->first->right, curr->first->right->val});
-		}
+// 		if (curr->second == 1 && curr->first->left != NULL) {
+// 			pair<TreeNode *, int> temp(curr->first->left, 0);
+// 			s.push(temp);
+// 			mp.insert({curr->first->left, curr->first->left->val});
+// 		} else if (curr->first->right != NULL) {
+// 			pair<TreeNode *, int> temp(curr->first->right, 0);
+// 			s.push(temp);
+// 			mp.insert({curr->first->right, curr->first->right->val});
+// 		}
+// 	}
+
+// 	return max;
+// }
+
+// int maxPathSum(TreeNode *root) {
+// 	if (root == NULL) {
+// 		return 0;
+// 	}
+
+// 	return traverse(root);
+// }
+
+int traverse(TreeNode *node, int *maxValue) {
+	if (node == NULL) {
+		return 0;
 	}
+	int left = traverse(node->left, maxValue);
+	int right = traverse(node->right, maxValue);
 
-	return max;
+	int retMax = max({left + node->val, right + node->val, node->val});
+
+	*maxValue = max({*maxValue, left + right + node->val, retMax});
+	return retMax;
 }
 
 int maxPathSum(TreeNode *root) {
@@ -111,7 +132,9 @@ int maxPathSum(TreeNode *root) {
 		return 0;
 	}
 
-	return traverse(root);
+	int maxValue = INT_MIN;
+	traverse(root, &maxValue);
+	return maxValue == INT_MIN ? 0 : maxValue;
 }
 
 int main() {
@@ -130,8 +153,6 @@ int main() {
 
 	TreeNode *node4 = new TreeNode(7);
 	node2->right = node4;
-
-	//
 
 	// head = new TreeNode(1);
 
