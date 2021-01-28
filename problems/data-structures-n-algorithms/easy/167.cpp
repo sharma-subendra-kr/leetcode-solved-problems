14c4d5b09a981ae8886831479dc5913772ac541e
Subendra Sharma <sharma.subendra.kr@gmail.com>
2021-01-28 21:59:02 +0530

    update

--- problems/data-structures-n-algorithms/easy/167.cpp
+++ problems/data-structures-n-algorithms/easy/167.cpp
@@ -10,22 +10,70 @@
 
 using namespace std;
 
-vector<int> twoSum(vector<int> &numbers, int target) {
-	unordered_map<int, int> mp;
+// All are valid answers
+// vector<int> twoSum(vector<int> &numbers, int target) {
+// 	int low = 0;
+// 	int high = numbers.size() - 1;
 
-	for (int i = 0; i < numbers.size(); i++) {
-		if (mp.find(target - numbers[i]) != mp.end()) {
-			vector<int> res;
-			res.push_back(mp.find(target - numbers[i])->second);
-			res.push_back(i + 1);
-			return res;
-		} else {
-			mp.insert({numbers[i], i + 1});
-		}
-	}
+// 	while (low < high) {
+// 		if (numbers[low] + numbers[high] == target) {
+// 			return vector<int>({low + 1, high + 1});
+// 		} else if (numbers[low] + numbers[high] > target) {
+// 			high--;
+// 		} else {
+// 			low++;
+// 		}
+// 	}
 
-	return vector<int>();
-}
+// 	return vector<int>();
+// }
+
+// Time limit exceeded
+// vector<int> twoSum(vector<int> &numbers, int target) {
+// 	int low = 0;
+// 	int high = numbers.size() - 1;
+// 	int i = 0;
+
+// 	while (i < numbers.size()) {
+// 		if (target - numbers[i] >= numbers[i]) {
+// 			low = i + 1;
+// 			high = numbers.size() - 1;
+// 		} else {
+// 			low = 0;
+// 			high = i - 1;
+// 		}
+// 		while (low <= high) {
+// 			int mid = (low + high) / 2;
+// 			if (numbers[i] + numbers[mid] == target) {
+// 				return vector<int>({i + 1, mid + 1});
+// 			} else if (numbers[i] + numbers[mid] > target) {
+// 				high--;
+// 			} else {
+// 				low++;
+// 			}
+// 		}
+// 		i++;
+// 	}
+
+// 	return vector<int>();
+// }
+
+// vector<int> twoSum(vector<int> &numbers, int target) {
+// 	unordered_map<int, int> mp;
+
+// 	for (int i = 0; i < numbers.size(); i++) {
+// 		if (mp.find(target - numbers[i]) != mp.end()) {
+// 			vector<int> res;
+// 			res.push_back(mp.find(target - numbers[i])->second);
+// 			res.push_back(i + 1);
+// 			return res;
+// 		} else {
+// 			mp.insert({numbers[i], i + 1});
+// 		}
+// 	}
+
+// 	return vector<int>();
+// }
 
 int main() {
 	vector<int> numbers;
