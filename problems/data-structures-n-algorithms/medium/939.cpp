513ddd28be0e1b0c5028f919d81c3b2b8586b3b0
Subendra Sharma <sharma.subendra.kr@gmail.com>
2021-02-28 20:29:54 +0530

    update

--- problems/data-structures-n-algorithms/medium/939.cpp
+++ problems/data-structures-n-algorithms/medium/939.cpp
@@ -11,100 +11,103 @@
 
 using namespace std;
 
-struct comparator {
-	bool operator()(vector<int> &a, vector<int> &b) {
-		//
-		if (a[1] != b[1]) {
-			return a[1] < b[1];
-		}
-		return a[0] < b[0];
-	}
-};
+// Timelimit exceeds
+// struct comparator {
+// 	bool operator()(vector<int> &a, vector<int> &b) {
+// 		//
+// 		if (a[1] != b[1]) {
+// 			return a[1] < b[1];
+// 		}
+// 		return a[0] < b[0];
+// 	}
+// };
 
-vector<vector<int>> getCoordinates(vector<int> &a, vector<int> &b,
-																	 vector<int> &c, vector<int> &d) {
-	vector<vector<int>> arr;
-	arr.push_back(a);
-	arr.push_back(b);
-	arr.push_back(c);
-	arr.push_back(d);
-	sort(arr.begin(), arr.end(), comparator());
+// vector<vector<int>> getCoordinates(vector<int> &a, vector<int> &b,
+// 																	 vector<int> &c, vector<int> &d) {
+// 	vector<vector<int>> arr;
+// 	arr.push_back(a);
+// 	arr.push_back(b);
+// 	arr.push_back(c);
+// 	arr.push_back(d);
+// 	sort(arr.begin(), arr.end(), comparator());
 
-	vector<int> temp = arr[3];
-	arr[3] = arr[2];
-	arr[2] = temp;
+// 	vector<int> temp = arr[3];
+// 	arr[3] = arr[2];
+// 	arr[2] = temp;
 
-	return arr;
-};
+// 	return arr;
+// };
 
-bool isRectangle(vector<vector<int>> &arr) {
-	if (arr[0][1] != arr[1][1] || arr[0][0] == arr[1][0] ||
-			arr[2][1] != arr[3][1] || arr[2][0] == arr[3][0]) {
-		return false;
-	}
-
-	if (arr[0][0] != arr[3][0] || arr[0][1] == arr[3][1] ||
-			arr[1][0] != arr[2][0] || arr[1][1] == arr[2][1]) {
-		return false;
-	}
-
-	return true;
-}
-
-// bool isRectangle(vector<int> &a, vector<int> &b, vector<int> &c,
-// 								 vector<int> &d) {
-// 	if (a[1] != b[1] || c[1] != d[1] || a[0] != d[0] || b[0] != c[0]) {
+// bool isRectangle(vector<vector<int>> &arr) {
+// 	if (arr[0][1] != arr[1][1] || arr[0][0] == arr[1][0] ||
+// 			arr[2][1] != arr[3][1] || arr[2][0] == arr[3][0]) {
 // 		return false;
 // 	}
+
+// 	if (arr[0][0] != arr[3][0] || arr[0][1] == arr[3][1] ||
+// 			arr[1][0] != arr[2][0] || arr[1][1] == arr[2][1]) {
+// 		return false;
+// 	}
+
 // 	return true;
 // }
 
-int getArea(vector<vector<int>> &arr) {
-	int length = arr[1][0] - arr[0][0];
-	int breadth = arr[3][1] - arr[0][1];
+// // bool isRectangle(vector<int> &a, vector<int> &b, vector<int> &c,
+// // 								 vector<int> &d) {
+// // 	if (a[1] != b[1] || c[1] != d[1] || a[0] != d[0] || b[0] != c[0]) {
+// // 		return false;
+// // 	}
+// // 	return true;
+// // }
 
-	return length * breadth;
-}
+// int getArea(vector<vector<int>> &arr) {
+// 	int length = arr[1][0] - arr[0][0];
+// 	int breadth = arr[3][1] - arr[0][1];
 
-int minAreaRect(vector<vector<int>> &points) {
-	//
-	if (points.size() < 4) {
-		return 0;
-	}
+// 	return length * breadth;
+// }
 
-	int MIN = INT_MAX;
+// int minAreaRect(vector<vector<int>> &points) {
+// 	//
+// 	if (points.size() < 4) {
+// 		return 0;
+// 	}
 
-	for (int i = 0; i < points.size() - 1; i++) {
-		for (int j = i + 1; j < points.size(); j++) {
-			if ((points[i][0] != points[j][0] && points[i][1] == points[j][1]) ||
-					(points[i][1] != points[j][1] && points[i][0] == points[j][0])) {
-				for (int k = 0; k < points.size() - 1; k++) {
-					if ((points[i][0] != points[k][0] && points[i][1] == points[k][1]) ||
-							(points[j][0] != points[k][0] && points[j][1] == points[k][1]) ||
-							(points[i][1] != points[k][1] && points[i][0] == points[k][0]) ||
-							(points[j][1] != points[k][1] && points[j][0] == points[k][0])) {
-						for (int l = k + 1; l < points.size(); l++) {
-							vector<vector<int>> rect =
-									getCoordinates(points[i], points[j], points[k], points[l]);
+// 	int MIN = INT_MAX;
 
-							if (isRectangle(rect)) {
-								int area = getArea(rect);
-								if (area < MIN) {
-									MIN = area;
-								}
-							}
-						}
-					}
-				}
-			}
-		}
-	}
+// 	for (int i = 0; i < points.size() - 1; i++) {
+// 		for (int j = i + 1; j < points.size(); j++) {
+// 			if ((points[i][0] != points[j][0] && points[i][1] == points[j][1]) ||
+// 					(points[i][1] != points[j][1] && points[i][0] == points[j][0])) {
+// 				for (int k = 0; k < points.size() - 1; k++) {
+// 					if ((points[i][0] != points[k][0] && points[i][1] == points[k][1])
+// || 							(points[j][0] != points[k][0] && points[j][1] ==
+// points[k][1])
+// || 							(points[i][1] != points[k][1] && points[i][0] ==
+// points[k][0])
+// || 							(points[j][1] != points[k][1] && points[j][0] ==
+// points[k][0])) { for (int l = k + 1; l < points.size(); l++) {
+// vector<vector<int>> rect = 									getCoordinates(points[i],
+// points[j], points[k], points[l]);
 
-	if (MIN != INT_MAX) {
-		return MIN;
-	}
-	return 0;
-}
+// 							if (isRectangle(rect)) {
+// 								int area = getArea(rect);
+// 								if (area < MIN) {
+// 									MIN = area;
+// 								}
+// 							}
+// 						}
+// 					}
+// 				}
+// 			}
+// 		}
+// 	}
+
+// 	if (MIN != INT_MAX) {
+// 		return MIN;
+// 	}
+// 	return 0;
+// }
 
 int main() {
 	//
