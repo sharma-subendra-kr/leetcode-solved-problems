6971516abac1f2d750716f45d17981d259498754
Subendra Sharma <sharma.subendra.kr@gmail.com>
2021-02-28 13:25:20 +0530

    update

---
+++ problems/data-structures-n-algorithms/medium/686.cpp
@@ -0,0 +1,45 @@
+// 686. Repeated String Match
+// https://leetcode.com/problems/repeated-string-match/
+
+#include <bits/stdc++.h>
+#include <deque>
+#include <iostream>
+#include <vector>
+#include <utility>
+#include <cstdlib>
+#include <math.h>
+
+using namespace std;
+
+int repeatedStringMatch(string a, string b) {
+	//
+	string copy = "";
+	int count = 0;
+	while (copy.size() < b.size()) {
+		copy += a;
+		count++;
+	}
+
+	if (copy.find(b) != string::npos) {
+		return count;
+	}
+	copy += a;
+	if (copy.find(b) != string::npos) {
+		return count + 1;
+	}
+	return -1;
+}
+
+int main() {
+	//
+	string a, b;
+
+	a = "abcd";
+	b = "cdabcdab";
+
+	int res = repeatedStringMatch(a, b);
+
+	cout << res << endl;
+
+	return 0;
+}
